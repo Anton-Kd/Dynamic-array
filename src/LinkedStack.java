f7698f438de65ca47ee4e4c22b3cb3b2df44dcf7
Anton_Kadyshev <fender1984@bk.ru>
2025-03-16 22:11:34 +0700

    Initail commit

---
+++ src/LinkedStack.java
@@ -0,0 +1,58 @@
+public class LinkedStack {
+    private Node tail; // ссылка на последний добавленный узел (обёртку)
+    private int size; // размер стека, т.е. количество элементов в нём
+
+    public void push(int value) {
+        Node node = new Node(value); // создаём новый узел
+        if (tail != null) { // если в стеке уже есть элементы
+            node.setPrev(tail); // связываем новый узел с последним
+        }
+        tail = node; // назначаем новый узел последним узлом
+        size++; // увеличиваем счётчик элементов
+    }
+
+    public int pop() {
+        int deletedValue = 0;
+        if (getSize() != 0) {
+            deletedValue = tail.getValue();
+            tail = tail.getPrev();
+            size--;
+        }
+        return deletedValue;
+    }
+    // ваш код
+    // возьмите value из последнего узла
+    // назначьте предыдущий к последнему узлу последним узлом
+
+    public int getSize() {
+        return size;
+        // ваш код
+        // верните размер стека
+    }
+
+    public boolean isEmpty() {
+        return getSize() == 0;
+        // ваш код
+        // верните ответ на вопрос, не пустой ли стек
+    }
+
+    public String toString() {
+        if (isEmpty()) {
+            return "EMPTY";
+        }
+        Node current = tail;
+        while (current != null) {
+            if (current.getPrev() == null) {
+                System.out.print(current.getValue());
+                break;
+            }
+            System.out.print(current.getValue() + " -> ");
+            current = current.getPrev();
+        }
+        return "";
+    }
+}
+// если есть элементы, пройдитесь по связному списку,
+// выводя элементы.
+// вывод должен быть в точности как в комментариях к main
+// при этом этот метод не должен менять стек!
